349067acba28a0e9121d4624254ab5a4d8191bf0
Emanuel Righetto <posta@emanuelrighetto.it>
2017-10-22 13:58:03 +0200

    Rimosso il controller in favore dei comandi drush

--- src/Commands/OperationCommands.php
+++ src/Commands/OperationCommands.php
@@ -10,19 +10,115 @@ use Drush\Commands\DrushCommands;
  * @package Drupal\node_operations\Commands
  */
 class OperationCommands extends DrushCommands {
-  
+
   /**
    * Fix Node Repository.
    *
-   * @command fix-node-repository
-   * @usage drush fnr
+   * @command nodeops:fix-node-repository
+   * @usage drush nos-fnr
    *  Fix Node Repository.
-   * @aliases fnr
+   * @aliases nos-fnr nodeops:fnr
    */
-  function fix_node_repository() {
+  public function fix_node_repository() {
     $current_field_storage_definitions = \Drupal::entityManager()
       ->getFieldStorageDefinitions('node');
     \Drupal::service('entity.last_installed_schema.repository')
       ->setLastInstalledFieldStorageDefinitions('node', $current_field_storage_definitions);
   }
+
+  /**
+   * Unset the node promoted value.
+   * So you can bulk remove the "promoted" flag to all node of some content type
+   *
+   * @command nodeops:promoted
+   * @param $node_type content type to look for.
+   * @usage drush nos-prom article
+   *  Unset the node promoted value.
+   * @aliases nos-prom
+   */
+  public function promoted($node_type) {
+
+    $query = \Drupal::entityQuery('node')->condition('type', $node_type);
+    $nids = $query->execute();
+
+    foreach ($nids as $nid) {
+      $node = Node::load($nid);
+      $node->promote->setValue(0);
+      $node->save();
+    }
+
+  }
+
+  /**
+   * Slug.
+   * After installing https://www.drupal.org/project/pathauto
+   * only the entities with flag "Generate automatic URL alias" set to true are affected on bulk operation.
+   * This operation change those flag and rebuild the path
+   *
+   * @command nodeops:path
+   * @param $lang language code to set.
+   * @usage drush nos-pat it
+   *  change pathauto flag and rebuild the path.
+   * @aliases nos-pat
+   */
+  public function slug($lang) {
+
+    $query = \Drupal::entityQuery('node')->condition('nid', 0,'>');
+    $nids = $query->execute();
+
+    foreach ($nids as $nid) {
+      /** @var \Drupal\node\Entity\Node $entity */
+      $entity = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+      $entity->path->pathauto = TRUE;
+
+      \Drupal::service('pathauto.generator')->updateEntityAlias($entity, 'update', ['language' => $lang]);
+      $entity->save();
+    }
+
+  }
+
+  /**
+   * Type.
+   * Change the type of a node.
+   * Pay attention: this can produce garbage in your db if content type do not support additional field related to the changing node
+   *
+   * @command nodeops:type
+   * @param $nid node id.
+   * @param $node_type to set.
+   * @usage drush nos-typ 9 article
+   *  Change the type of a node.
+   * @aliases nos-typ
+   */
+  public function type($nid,$node_type) {
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = Node::load($nid);
+    $node->type->setValue($node_type);
+    $node->save();
+
+  }
+
+  /**
+   * Lang.
+   * Set a consistent language of those node where language is set to UND
+   *
+   * @command nodeops:lang
+   * @param $lang language code to set.
+   * @usage drush nos-lan it
+   *  Unset the node promoted value.
+   * @aliases nos-lan
+   */
+  public function lang($lang) {
+
+    $query = \Drupal::entityQuery('node')->condition('langcode','und');
+    $nids = $query->execute();
+
+    foreach ($nids as $nid) {
+      /** @var \Drupal\node\Entity\Node $node */
+      $node = Node::load($nid);
+      $node->langcode->setValue($lang);
+      $node->save();
+    }
+
+  }
+
 }
